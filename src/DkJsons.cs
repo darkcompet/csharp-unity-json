9a41e4ca32e8cd86d43a7615963218182a5ee2a8
DarkCompet <darkcompet@gmail.com>
2022-04-01 15:38:33 +0700

    Use newtonsoft json as default

--- src/DkJsons.cs
+++ src/DkJsons.cs
@@ -1,9 +1,6 @@
 namespace Tool.Compet.Json {
-#if HAS_NEWTONSOFT_JSON
 	using Newtonsoft.Json;
-#else
 	using UnityEngine;
-#endif
 
 	/// Json helper for converter Json vs Object.
 	/// See https://www.jacksondunstan.com/articles/3303 to choose best approach on each serialize/deserialize.
@@ -12,8 +9,6 @@ namespace Tool.Compet.Json {
 	/// - Deserialize small string => Unity is optimal
 	/// - Deserialize large string => Newton is optimal
 	public class DkJsons {
-/// Use Newtonsoft Json
-#if HAS_NEWTONSOFT_JSON
 		public static string Obj2Json(object serializableObj, Formatting formatting = Formatting.None) {
 			return JsonConvert.SerializeObject(serializableObj, formatting);
 		}
@@ -22,16 +17,14 @@ namespace Tool.Compet.Json {
 		public static T Json2Obj<T>(string json) where T : class {
 			return JsonConvert.DeserializeObject<T>(json);
 		}
-/// Use Unity Json
-#else
-		public static string Obj2Json(object serializableObj, bool prettyPrint = false) {
-			return JsonUtility.ToJson(serializableObj, prettyPrint);
-		}
 
-		/// Caller should add `where T : class` to its function to allow return nullable-result.
-		public static T Json2Obj<T>(string json) where T : class {
-			return JsonUtility.FromJson<T>(json);
-		}
-#endif
+		// public static string Obj2Json(object serializableObj, bool prettyPrint = false) {
+		// 	return JsonUtility.ToJson(serializableObj, prettyPrint);
+		// }
+
+		// /// Caller should add `where T : class` to its function to allow return nullable-result.
+		// public static T Json2Obj<T>(string json) where T : class {
+		// 	return JsonUtility.FromJson<T>(json);
+		// }
 	}
 }
